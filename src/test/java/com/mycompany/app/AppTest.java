3a84fffe4c74a5652c3d9beb2507b409cff23a72
Abhishek Konduri <abhishek.konduri@sjsu.edu>
2020-05-15 17:37:02 -0700

    ID-11 New Issue

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -43,6 +43,21 @@ public class AppTest
         }
     }
 
+    /**
+     * Test case
+     */
+    @Test
+    public void testAppMainNewTest()
+    {
+        App.main(null);
+        try {
+            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+        } catch (AssertionError e) {
+            fail("\"message\" is not \"Hello World!\"");
+        }
+    }
+
+
     @After
     public void cleanUpStreams() {
         System.setOut(null);
